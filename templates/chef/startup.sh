37c60c0fbbd0de5256bcd121b30fc980692d4d6c
Jacob Lewallen <jlewallen@gmail.com>
2011-04-18 18:44:44 -0700

    Pull configuration from S3.

--- templates/chef/startup.sh
+++ templates/chef/startup.sh
@@ -5,7 +5,7 @@ set -e -x
 LEXY=/lexy
 
 apt-get  update -q -y
-apt-get install -q -y rsync git-core
+apt-get install -q -y rsync git-core curl
 apt-get install -q -y ruby ruby-dev rubygems rake
 apt-get install -q -y build-essential
 
@@ -13,9 +13,10 @@ gem install rdoc chef ohai --no-ri --no-rdoc --source http://gems.opscode.com --
 
 if ! [ -d /chef ]; then
   if ! [ -d $LEXY ]; then
-    git clone git://github.com/jlewallen/lexy.git $LEXY
+    curl http://sp-provisioning.s3.amazonaws.com/chef.tar.gz > /tmp/chef.tar.gz
+    tar xzf /tmp/chef.tar.gz -C /
+    # git clone git://github.com/jlewallen/lexy.git $LEXY
   fi
-
   ln -sf $LEXY/chef /chef
 fi
 
